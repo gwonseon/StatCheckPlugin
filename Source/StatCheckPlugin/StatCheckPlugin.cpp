1892fa77cf7c866403dd9aff40ee1ef169da7d5f
권선 <97091329+gwonseon@users.noreply.github.com>
2026-05-25 13:56:32 +0900

    Initial commit

---
+++ Source/StatCheckPlugin/StatCheckPlugin.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StatCheckPlugin.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StatCheckPlugin, "StatCheckPlugin" );
